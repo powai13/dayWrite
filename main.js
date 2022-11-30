1452dcbe0498da0356398bb9fcf97c9dddcaa98f
powai13 <76779356+powai13@users.noreply.github.com>
2022-11-30 11:41:32 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-'use strict'
+function csv2Array(filePath) { //csvﾌｧｲﾙﾉ相対ﾊﾟｽor絶対ﾊﾟｽ
+	var csvData = new Array();
+	var data = new XMLHttpRequest();	
+	data.open("GET", filePath, false); //true:非同期,false:同期
+	data.send(null);
 
-window.onload = function() {
-
-    /**
-     * グローバル宣言
-     */
-
-    //画面のサイズを合わせる
-    function autoSize() {
-        const main = document.getElementById('main');
-        main.width = window.innerWidth + "px";
-    }
-
-    autoSize();
-    window.addEventListener('resize', autoSize);
+	var LF = String.fromCharCode(10); //改行ｺｰﾄﾞ
+	var lines = data.responseText.split(LF);
+	for (var i = 0; i < lines.length;++i) {
+		var cells = lines[i].split(",");
+		if( cells.length != 1 ) {
+			csvData.push(cells);
+		}
+	}
+	return csvData;
 }
